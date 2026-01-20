96c8c8d2f3f6c9d13b57423211578632619c8c70
Quantumpepe <fubulubu3030@gmail.com>
2026-01-20 23:27:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { PrivyProvider, usePrivy, useWallets } from "@privy-io/react-auth";
+
 import { Alchemy, Network, Utils } from "alchemy-sdk";
 
 import "./App.css";

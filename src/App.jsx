b978bfcfc84a42b2962e83578dbb7922a87fff1e
Quantumpepe <fubulubu3030@gmail.com>
2026-03-30 18:16:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6897,7 +6897,7 @@ const vaultFreeQty = useMemo(
                       disabled={aiExplainLoading}
                       title={!isPro ? "Subscribe to Nexus Pro to use AI" : ""}
                     >
-                      {aiExplainLoading ? "Thinking…" : (isPro ? "AI Insight new" : "Pro required")}
+                      {aiExplainLoading ? "Thinking…" : (isPro ? "AI Insight new1" : "Pro required")}
                     </button>
                   </div>
                   {aiExplainData ? (
